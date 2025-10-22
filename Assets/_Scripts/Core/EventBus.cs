ae975bbff16d67ba6326aebaeced70878132bb44
Bagayevets <ka4ergd@gmail.com>
2025-10-22 23:30:19 +0300

    no message

--- Assets/_Scripts/Core/EventBus.cs
+++ Assets/_Scripts/Core/EventBus.cs
@@ -9,7 +9,17 @@ public static class EventBus
 
     // Метод для подписки на событие определенного типа
     public static void Subscribe<T>(Action<T> listener) where T : GameEvent //бланк с пустым полем <Тип события> (метод-обработчик который получит событие типа T)  T всегда будет событием (наследником GameEvent)
-    { 
-    
+    {
+        Type eventType = typeof(T); // Помещаем в eventType информацию о типе данных
+
+        // Проверка на существование нужного списка, если нет, то добавит
+        if (!_eventListeners.ContainsKey(eventType))
+        {
+            _eventListeners[eventType] = new List<Action<GameEvent>>();
+        }
+
+
+        Action<GameEvent> wrappedListener = (e) => listener((T)e);  // объявляется переменная типа Action<GameEvent>в которую помещаем события типа "e" и пребразовываем в тип "T"
+        _eventListeners[eventType].Add(wrappedListener);// Добавляем нового подписчика в список подписчиков на это событие
     }
 }
